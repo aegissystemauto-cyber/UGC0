6c7d032a0674f409f18bdd78dfebb0ced6ef941f
aegissystemauto-cyber <aegissystemauto@gmail.com>
2026-04-22 19:39:30 +0700

    Update main.py

--- UGC-Platform/app/main.py
+++ UGC-Platform/app/main.py
@@ -1,1 +1,13 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
+app = FastAPI(title="Đế chế UGC của Wy")
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse(
+        "index.html", 
+        {"request": request, "message": "Chào mừng đến với Bãi Rác Kỹ Thuật Số (có tổ chức) của Wy!"}
+    )
