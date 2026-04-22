134aeda8547c5e0d021a371ca38356d9f5944c0e
aegissystemauto-cyber <aegissystemauto@gmail.com>
2026-04-22 20:36:32 +0700

    Update main.py

--- UGC-Platform/app/main.py
+++ UGC-Platform/app/main.py
@@ -1,14 +1,25 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
+from . import models, database
+
+# Tạo bảng dữ liệu
+models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI(title="Đế chế UGC của Wy")
 templates = Jinja2Templates(directory="app/templates")
 
 @app.get("/", response_class=HTMLResponse)
-async def read_root(request: Request):
+async def read_root(request: Request, db: Session = Depends(database.get_db)):
+    # Lấy thử danh sách danh mục (hiện tại chắc chắn đang rỗng)
+    categories = db.query(models.Category).all()
+    
     return templates.TemplateResponse(
         request=request, 
         name="index.html", 
-        context={"message": "Chào mừng đến với Bãi Rác Kỹ Thuật Số (có tổ chức) của Wy!"}
+        context={
+            "message": "Hệ thống Database đã sẵn sàng khai hỏa!",
+            "categories": categories
+        }
     )
