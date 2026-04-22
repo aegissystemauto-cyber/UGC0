46650ac39971000c4c5b20c82686701030c27788
aegissystemauto-cyber <aegissystemauto@gmail.com>
2026-04-22 20:35:35 +0700

    Update database.py

--- UGC-Platform/app/database.py
+++ UGC-Platform/app/database.py
@@ -1,1 +1,21 @@
+from sqlalchemy import create_all, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
+# Tạo file database tên là sql_app.db ngay trong thư mục
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+# Hàm để lấy phiên làm việc với DB
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
